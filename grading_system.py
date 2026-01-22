2c966cefd0669dcecc3c2cd4f255aa5cce279c10
Tobiloba405 <oluwatnade07@gmail.com>
2026-01-22 22:54:16 +0100

    Create  grading_system.py

---
+++ grading_system.py
@@ -0,0 +1,10 @@
+print("STUDENT REGISTRATION SYSTEM")
+
+name = input("Enter student name: ")
+matric_no = input("Enter matric number: ")
+department = input("Enter department: ")
+
+print("\n--- Student Details ---")
+print("Name:", name)
+print("Matric Number:", matric_no)
+print("Department:", department)
